9567e2dcb768c3edfb9749ea97438710b5bb63e5
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-06 10:19:23 +0530

    get post by id and raising http exceptions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from  fastapi import FastAPI
 from fastapi.params import Body
+from fastapi import HTTPException
 from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
 app = FastAPI()
 
 
@@ -9,13 +13,42 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
-     
-
-@app.get("/")
-def read_root():
-    return {  "World1"}
+    published: bool = True
+    rating:Optional[float] = None
 
 
+my_posts = [
+    {"id": 1, "title": "First post", "content": "Hello world!", "published": True, "rating": 4.5},
+    {"id": 2, "title": "Second post", "content": "FastAPI is awesome!", "published": False}
+]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+    return None
+
+@app.get("/posts")
+def get_posts():
+    return { "data": my_posts }
+
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+        
+    return {"post_detail": post}
+
+  
 @app.post("/posts")
 def create_post(post: Post):
-    return {"post": post['title']}
+    print(post)
+    print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data":my_posts[-1]}
