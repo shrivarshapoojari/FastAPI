737ce6a9d03b6db40084408d371b07149d937563
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-06 09:27:35 +0530

    validating with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from  fastapi import FastAPI
-
+from fastapi.params import Body
+from pydantic import BaseModel
 app = FastAPI()
+
+
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+     
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {  "World1"}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    return {"post": post['title']}
