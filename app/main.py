eddd330cac3f8b051f76508097d8ecd4d0c062ca
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-06 17:23:00 +0530

    all methods converted to db

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ while True:
     except Exception as e:
         print("Error while connecting to PostgreSQL:", e)
         time.sleep(2)
+        
 
  
  
@@ -40,22 +41,23 @@ my_posts = [
     {"id": 2, "title": "Second post", "content": "FastAPI is awesome!", "published": False}
 ]
 
-
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-    return None
+ 
 
 @app.get("/posts")
 def get_posts():
-    return { "data": my_posts }
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
+    
 
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+
+    post = cursor.fetchone()
+    conn.commit()
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
         
@@ -64,23 +66,43 @@ def get_post(id: int):
   
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    print(post)
-    print(post.dict())
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data":my_posts[-1]}
+
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)  RETURNING *""",(post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+     
+   
+    return {"data":new_post}
+
+
+
+
+
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if not updated_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+        
+    return {"data": updated_post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = 0
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            index = i
-            break
-    if index == 0:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+
+
+ 
+    if deleted_post is None:
         raise HTTPException(status_code=404, detail="Post not found")
-    
-    my_posts.pop(index)
+     
     return {"message": "Post deleted successfully"}
