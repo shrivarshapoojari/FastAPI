0b62316bbaf3ab33adf99a086aa9b637b6a7d675
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-11 21:32:05 +0530

    alembic added, cors added

--- app/main.py
+++ app/main.py
@@ -10,21 +10,29 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from typing import List  
 from . import utils
-from  .router import post, user,auth
+from  .router import post, user,auth,vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
-
-print(settings.database_password)
+ 
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
